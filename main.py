ee1a33be74c26aad5c2aec815e0d9f5a46be4932
KayMohh <muhammadkadir022@gmail.com>
2025-02-06 08:29:51 -0800

    Update main.py
    added jsonresponses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import requests
+from fastapi.responses import JSONResponse
 
 def is_prime(n: int) -> bool:
     if n < 2:
